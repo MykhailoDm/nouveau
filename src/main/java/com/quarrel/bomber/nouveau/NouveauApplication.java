09963746a3246a1792b2311d0b2c3962370389d5
MykhailoDm <mikhailo2002dm@gmail.com>
2022-02-26 17:35:42 +0200

    Initial commit

---
+++ src/main/java/com/quarrel/bomber/nouveau/NouveauApplication.java
@@ -0,0 +1,13 @@
+package com.quarrel.bomber.nouveau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NouveauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NouveauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quarrel/bomber/nouveau/NouveauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quarrel.bomber.nouveau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NouveauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
